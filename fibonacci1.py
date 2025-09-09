9a2b6ead43062baf4f5d6f89926a05dc9ff366df
Gagan S R <srgagan99@gmail.com>
2025-09-09 08:43:43 +0530

    Create fibonacci1.py

---
+++ fibonacci1.py
@@ -0,0 +1,12 @@
+# Fibonacci using recursion
+
+def fib(n):
+    if n <= 1:
+        return n
+    else:
+        return fib(n-1) + fib(n-2)
+
+n = int(input("Enter number of terms: "))
+print("Fibonacci series:")
+for i in range(n):
+    print(fib(i), end=" ")
